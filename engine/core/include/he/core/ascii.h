f059c8e7d0db85021256e03b4abb01ee46184ef6
Chad Engler <englercj@live.com>
2021-07-08 08:35:01 -0700

    Add some ascii helpers

---
+++ engine/core/include/he/core/ascii.h
@@ -0,0 +1,158 @@
+#pragma once
+
+#include "he/core/types.h"
+
+namespace he
+{
+    /// Checks if the character is whitespace (space, tab, newline, etc)
+    ///
+    /// \param c The character to check.
+    /// \return True if the character is a whitespace character.
+    constexpr bool IsWhitespace(char c);
+
+    /// Checks if the null terminated string contains only whitespace characters.
+    ///
+    /// \param str The string to check.
+    /// \return True if the string contains only whitespace characters.
+    constexpr bool IsWhitespace(const char* str);
+
+    /// Checks if the character is uppercase.
+    ///
+    /// \param c The character to check.
+    /// \return True if the character is in the range `[A,Z]`.
+    constexpr bool IsUpper(char c);
+
+    /// Checks if the null terminated string contains only uppercase characters.
+    ///
+    /// \param str The string to check.
+    /// \return True if the string contains only characters in the range `[A,Z]`.
+    constexpr bool IsUpper(const char* str);
+
+    /// Checks if the character is lowercase.
+    ///
+    /// \param c The character to check.
+    /// \return True if the character is in the range `[a,z]`.
+    constexpr bool IsLower(char c);
+
+    /// Checks if the null terminated string contains only lowercase characters.
+    ///
+    /// \param str The string to check.
+    /// \return True if the string contains only characters in the range `[a,z]`.
+    constexpr bool IsLower(const char* str);
+
+    /// Checks if the character is an alphabet character.
+    ///
+    /// \param c The character to check.
+    /// \return True if the character is in the range `[a,z]` or `[A,Z]`.
+    constexpr bool IsAlpha(char c);
+
+    /// Checks if the null terminated string contains only alphabet characters.
+    ///
+    /// \param str The string to check.
+    /// \return True if the string contains only characters in the range `[a,z]` or `[A,Z]`.
+    constexpr bool IsAlpha(const char* str);
+
+    /// Checks if the character is a numeric digit.
+    ///
+    /// \param c The character to check.
+    /// \return True if the character is in the range `[0,9]`.
+    constexpr bool IsNumeric(char c);
+
+    /// Checks if the null terminated string contains only numeric digits.
+    ///
+    /// \param str The string to check.
+    /// \return True if the string contains only characters in the range `[0,9]`.
+    constexpr bool IsNumeric(const char* str);
+
+    /// Checks if the character is an alphabet character or numeric digit.
+    ///
+    /// \param c The character to check.
+    /// \return True if the character is in the range `[a,z]`, `[A,Z]`, or `[0,9]`.
+    constexpr bool IsAlphaNum(char c);
+
+    /// Checks if the null terminated string contains only alphabet characters or numeric digits.
+    ///
+    /// \param str The string to check.
+    /// \return True if the string contains only characters in the range `[a,z]`, `[A,Z]`, or `[0,9]`.
+    constexpr bool IsAlphaNum(const char* str);
+
+    /// Checks if the null terminated string contains only numeric digits, and optional leading dash.
+    ///
+    /// \param str The string to check.
+    /// \return True if the string contains only characters in the range `[0,9]`, and an optional leading `-`.
+    constexpr bool IsIntegral(const char* str);
+
+    /// Checks if the null terminated string contains only numeric digits, a single period, and optional leading dash.
+    ///
+    /// \param str The string to check.
+    /// \return True if the string contains only characters in the range `[0,9]`, a single `.`, and an optional leading `-`.
+    constexpr bool IsFloatingPoint(const char* str);
+
+    /// Checks if the character is a hexadecimal digit.
+    ///
+    /// \param c The character to check.
+    /// \return True if the character is in the range `[a,f]`, `[A,F]`, or `[0,9]`.
+    constexpr bool IsHex(char c);
+
+    /// Checks if the null terminated string contains only hexadecimal digits.
+    ///
+    /// \param str The string to check.
+    /// \return True if the string contains only characters in the range `[a,f]`, `[A,F]`, or `[0,9]`.
+    constexpr bool IsHex(const char* str);
+
+    /// Checks if the character is printable.
+    ///
+    /// \param c The character to check.
+    /// \return True if the character is in the range `[ ,~]`.
+    constexpr bool IsPrint(char c);
+
+    /// Checks if the null terminated string contains only printable characters.
+    ///
+    /// \param str The string to check.
+    /// \return True if the string contains only characters in the range `[ ,~]`.
+    constexpr bool IsPrint(const char* str);
+
+    /// Transforms the character to uppercase.
+    ///
+    /// \param c The character to transform.
+    /// \return The transformed character.
+    constexpr char ToUpper(char c);
+
+    /// Transforms the characters in the null terminated string to uppercase.
+    ///
+    /// \param str The string to transform.
+    constexpr void ToUpper(char* str);
+
+    /// Transforms the character to lowercase.
+    ///
+    /// \param c The character to check.
+    /// \return The transformed character.
+    constexpr char ToLower(char c);
+
+    /// Transforms the characters in the null terminated string to lowercase.
+    ///
+    /// \param str The string to transform.
+    constexpr void ToLower(char* str);
+
+    /// Transforms `nibble` to the ascii hex digit that represents it.
+    ///
+    /// \param nibble The value to transform into a hex digit. Must be in the range `[0, 16)`.
+    /// \param upperCase When true returns an uppercase hex digit, lowercase otherwise.
+    /// \return The hex digit that represents the value, or the null character ('\0') on invalid input.
+    constexpr char ToHex(uint8_t nibble, bool upperCase = false);
+
+    /// Gets the value of the hex digit.
+    ///
+    /// \param c The hex digit to get the value for.
+    /// \return The nibble this hex digit represents. Characters that are not valid hex will return zero.
+    constexpr uint8_t HexToNibble(char c);
+
+    /// Gets the value of a pair hex digits.
+    ///
+    /// \param a The first hex digit in the pair.
+    /// \param b The second hex digit in the pair.
+    /// \return The byte this pair of hex digits represents. Characters that are not valid hex will return zero.
+    constexpr uint8_t HexPairToByte(char a, char b);
+}
+
+#include "he/core/inline/ascii.inl"
---
+++ engine/core/include/he/core/inline/ascii.inl
@@ -0,0 +1,187 @@
+// Copyright Chad Engler
+
+namespace he
+{
+    template <bool (*fn)(char c)>
+    constexpr bool _IsCharTest(const char* str)
+    {
+        while (*str)
+        {
+            if (!fn(*str))
+                return false;
+
+            ++str;
+        }
+        return true;
+    }
+
+    constexpr bool IsWhitespace(char c)
+    {
+        return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r';
+    }
+
+    constexpr bool IsWhitespace(const char* str)
+    {
+        return _IsCharTest<IsWhitespace>(str);
+    }
+
+    constexpr bool IsUpper(char c)
+    {
+        return c >= 'A' && c <= 'Z';
+    }
+
+    constexpr bool IsUpper(const char* str)
+    {
+        return _IsCharTest<IsUpper>(str);
+    }
+
+    constexpr bool IsLower(char c)
+    {
+        return c >= 'a' && c <= 'z';
+    }
+
+    constexpr bool IsLower(const char* str)
+    {
+        return _IsCharTest<IsLower>(str);
+    }
+
+    constexpr bool IsAlpha(char c)
+    {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+
+    constexpr bool IsAlpha(const char* str)
+    {
+        return _IsCharTest<IsAlpha>(str);
+    }
+
+    constexpr bool IsNumeric(char c)
+    {
+        return c >= '0' && c <= '9';
+    }
+
+    constexpr bool IsNumeric(const char* str)
+    {
+        return _IsCharTest<IsNumeric>(str);
+    }
+
+    constexpr bool IsAlphaNum(char c)
+    {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+    }
+
+    constexpr bool IsAlphaNum(const char* str)
+    {
+        return _IsCharTest<IsAlphaNum>(str);
+    }
+
+    constexpr bool IsIntegral(const char* str)
+    {
+        if (*str == '-')
+            str++;
+
+        return IsNumeric(str);
+    }
+
+    constexpr bool IsFloatingPoint(const char* str)
+    {
+        if (*str == '-')
+            str++;
+
+        bool seenPeriod = false;
+
+        while (*str)
+        {
+            if (!IsNumeric(*str))
+            {
+                if (*str == '.')
+                {
+                    if (seenPeriod)
+                        return false;
+                    seenPeriod = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            ++str;
+        }
+        return true;
+    }
+
+    constexpr bool IsHex(char c)
+    {
+        return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
+    }
+
+    constexpr bool IsHex(const char* str)
+    {
+        return _IsCharTest<IsHex>(str);
+    }
+
+    constexpr bool IsPrint(char c)
+    {
+        return c >= ' ' && c <= '~';
+    }
+
+    constexpr bool IsPrint(const char* str)
+    {
+        return _IsCharTest<IsPrint>(str);
+    }
+
+    constexpr char ToUpper(char c)
+    {
+        return IsLower(c) ? c + 'A' - 'a' : c;
+    }
+
+    constexpr void ToUpper(char* str)
+    {
+        while (*str)
+        {
+            *str = ToUpper(*str);
+            ++str;
+        }
+    }
+
+    constexpr char ToLower(char c)
+    {
+        return IsUpper(c) ? c + 'a' - 'A' : c;
+    }
+
+    constexpr void ToLower(char* str)
+    {
+        while (*str)
+        {
+            *str = ToLower(*str);
+            ++str;
+        }
+    }
+
+    constexpr char ToHex(uint8_t nibble, bool upperCase)
+    {
+        constexpr char HexDigits[] = "0123456789abcdef";
+
+        if (nibble < (sizeof(HexDigits) - 1))
+        {
+            char digit = HexDigits[nibble];
+            return upperCase ? ToUpper(digit) : digit;
+        }
+
+        return '\0';
+    }
+
+    constexpr uint8_t HexToNibble(char c)
+    {
+        return (c >= '0' && c <= '9') ? c - '0'
+            : c >= 'a' && c <= 'f' ? 10 + c - 'a'
+            : c >= 'A' && c <= 'F' ? 10 + c - 'A'
+            : 0;
+    }
+
+    constexpr uint8_t HexPairToByte(char a, char b)
+    {
+        return HexToNibble(a) * 16 + HexToNibble(b);
+    }
+}

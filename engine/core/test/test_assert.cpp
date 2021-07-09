729738ad2bf3cce4deba0a7209c4c09c19fca82f
Chad Engler <englercj@live.com>
2021-07-09 09:10:48 -0700

    Add more test stubs

---
+++ engine/core/test/test_assert.cpp
@@ -0,0 +1,17 @@
+// Copyright Chad Engler
+
+#include "he/core/assert.h"
+
+#include "he/core/test.h"
+
+using namespace he;
+
+// ------------------------------------------------------------------------------------------------
+HE_TEST(core, assert, ASSERT)
+{
+}
+
+// ------------------------------------------------------------------------------------------------
+HE_TEST(core, assert, VERIFY)
+{
+}
---
+++ engine/core/test/test_debug.cpp
@@ -0,0 +1,17 @@
+// Copyright Chad Engler
+
+#include "he/core/allocator.h"
+
+#include "he/core/test.h"
+
+using namespace he;
+
+// ------------------------------------------------------------------------------------------------
+HE_TEST(core, debug, OutputToDebugger)
+{
+}
+
+// ------------------------------------------------------------------------------------------------
+HE_TEST(core, debug, IsDebuggerAttached)
+{
+}

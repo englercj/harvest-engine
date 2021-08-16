3c944ab7a07ac535771848fb5d6e6f73bb490d3e
Chad Engler <englercj@live.com>
2021-08-15 19:43:12 -0700

    Initializer style fixes

--- engine/math/include/he/math/constants.h
+++ engine/math/include/he/math/constants.h
@@ -23,52 +23,52 @@ namespace he
     inline constexpr float Float_ZeroSafe = 1000.0f * Float_Min;
 
     inline constexpr Vec4a Vec4a_Infinity{ Float_Infinity, Float_Infinity, Float_Infinity, Float_Infinity };
-    inline constexpr Vec4a Vec4a_Zero = { 0.0f, 0.0f, 0.0f, 0.0f };
-    inline constexpr Vec4a Vec4a_One = { 1.0f, 1.0f, 1.0f, 1.0f };
-    inline constexpr Vec4a Vec4a_X = { 1.0f, 0.0f, 0.0f, 0.0f };
-    inline constexpr Vec4a Vec4a_Y = { 0.0f, 1.0f, 0.0f, 0.0f };
-    inline constexpr Vec4a Vec4a_Z = { 0.0f, 0.0f, 1.0f, 0.0f };
-    inline constexpr Vec4a Vec4a_W = { 0.0f, 0.0f, 0.0f, 1.0f };
+    inline constexpr Vec4a Vec4a_Zero{ 0.0f, 0.0f, 0.0f, 0.0f };
+    inline constexpr Vec4a Vec4a_One{ 1.0f, 1.0f, 1.0f, 1.0f };
+    inline constexpr Vec4a Vec4a_X{ 1.0f, 0.0f, 0.0f, 0.0f };
+    inline constexpr Vec4a Vec4a_Y{ 0.0f, 1.0f, 0.0f, 0.0f };
+    inline constexpr Vec4a Vec4a_Z{ 0.0f, 0.0f, 1.0f, 0.0f };
+    inline constexpr Vec4a Vec4a_W{ 0.0f, 0.0f, 0.0f, 1.0f };
 
-    inline constexpr Vec2f Vec2f_Zero = { 0, 0 };
-    inline constexpr Vec2f Vec2f_One = { 1, 1, };
-    inline constexpr Vec2f Vec2f_X = { 1, 0 };
-    inline constexpr Vec2f Vec2f_Y = { 0, 1 };
+    inline constexpr Vec2f Vec2f_Zero{ 0, 0 };
+    inline constexpr Vec2f Vec2f_One{ 1, 1, };
+    inline constexpr Vec2f Vec2f_X{ 1, 0 };
+    inline constexpr Vec2f Vec2f_Y{ 0, 1 };
 
-    inline constexpr Vec3f Vec3f_Zero = { 0, 0, 0 };
-    inline constexpr Vec3f Vec3f_One = { 1, 1, 1 };
-    inline constexpr Vec3f Vec3f_X = { 1, 0, 0 };
-    inline constexpr Vec3f Vec3f_Y = { 0, 1, 0 };
-    inline constexpr Vec3f Vec3f_Z = { 0, 0, 1 };
+    inline constexpr Vec3f Vec3f_Zero{ 0, 0, 0 };
+    inline constexpr Vec3f Vec3f_One{ 1, 1, 1 };
+    inline constexpr Vec3f Vec3f_X{ 1, 0, 0 };
+    inline constexpr Vec3f Vec3f_Y{ 0, 1, 0 };
+    inline constexpr Vec3f Vec3f_Z{ 0, 0, 1 };
 
-    inline constexpr Vec4f Vec4f_Zero = { 0, 0, 0, 0 };
-    inline constexpr Vec4f Vec4f_One = { 1, 1, 1, 1 };
-    inline constexpr Vec4f Vec4f_X = { 1, 0, 0, 0 };
-    inline constexpr Vec4f Vec4f_Y = { 0, 1, 0, 0 };
-    inline constexpr Vec4f Vec4f_Z = { 0, 0, 1, 0 };
-    inline constexpr Vec4f Vec4f_W = { 0, 0, 0, 1 };
+    inline constexpr Vec4f Vec4f_Zero{ 0, 0, 0, 0 };
+    inline constexpr Vec4f Vec4f_One{ 1, 1, 1, 1 };
+    inline constexpr Vec4f Vec4f_X{ 1, 0, 0, 0 };
+    inline constexpr Vec4f Vec4f_Y{ 0, 1, 0, 0 };
+    inline constexpr Vec4f Vec4f_Z{ 0, 0, 1, 0 };
+    inline constexpr Vec4f Vec4f_W{ 0, 0, 0, 1 };
 
-    inline constexpr Vec2i Vec2i_Zero = { 0, 0 };
-    inline constexpr Vec2i Vec2i_One = { 1, 1 };
-    inline constexpr Vec2i Vec2i_X = { 1, 0 };
-    inline constexpr Vec2i Vec2i_Y = { 0, 1 };
+    inline constexpr Vec2i Vec2i_Zero{ 0, 0 };
+    inline constexpr Vec2i Vec2i_One{ 1, 1 };
+    inline constexpr Vec2i Vec2i_X{ 1, 0 };
+    inline constexpr Vec2i Vec2i_Y{ 0, 1 };
 
-    inline constexpr Vec3i Vec3i_Zero = { 0, 0, 0 };
-    inline constexpr Vec3i Vec3i_One = { 1, 1, 1 };
-    inline constexpr Vec3i Vec3i_X = { 1, 0, 0 };
-    inline constexpr Vec3i Vec3i_Y = { 0, 1, 0 };
-    inline constexpr Vec3i Vec3i_Z = { 0, 0, 1 };
+    inline constexpr Vec3i Vec3i_Zero{ 0, 0, 0 };
+    inline constexpr Vec3i Vec3i_One{ 1, 1, 1 };
+    inline constexpr Vec3i Vec3i_X{ 1, 0, 0 };
+    inline constexpr Vec3i Vec3i_Y{ 0, 1, 0 };
+    inline constexpr Vec3i Vec3i_Z{ 0, 0, 1 };
 
-    inline constexpr Vec4i Vec4i_Zero = { 0, 0, 0, 0 };
-    inline constexpr Vec4i Vec4i_One = { 1, 1, 1, 1 };
-    inline constexpr Vec4i Vec4i_X = { 1, 0, 0, 0 };
-    inline constexpr Vec4i Vec4i_Y = { 0, 1, 0, 0 };
-    inline constexpr Vec4i Vec4i_Z = { 0, 0, 1, 0 };
-    inline constexpr Vec4i Vec4i_W = { 0, 0, 0, 1 };
+    inline constexpr Vec4i Vec4i_Zero{ 0, 0, 0, 0 };
+    inline constexpr Vec4i Vec4i_One{ 1, 1, 1, 1 };
+    inline constexpr Vec4i Vec4i_X{ 1, 0, 0, 0 };
+    inline constexpr Vec4i Vec4i_Y{ 0, 1, 0, 0 };
+    inline constexpr Vec4i Vec4i_Z{ 0, 0, 1, 0 };
+    inline constexpr Vec4i Vec4i_W{ 0, 0, 0, 1 };
 
-    inline constexpr Quat Quat_Identity = { 0.0f, 0.0f, 0.0f, 1.0f };
-    inline constexpr Quata Quata_Identity = { 0.0f, 0.0f, 0.0f, 1.0f };
+    inline constexpr Quat Quat_Identity{ 0.0f, 0.0f, 0.0f, 1.0f };
+    inline constexpr Quata Quata_Identity{ 0.0f, 0.0f, 0.0f, 1.0f };
 
-    inline constexpr Mat44 Mat44_Zero = { Vec4a_Zero, Vec4a_Zero, Vec4a_Zero, Vec4a_Zero };
-    inline constexpr Mat44 Mat44_Identity = { Vec4a_X, Vec4a_Y, Vec4a_Z, Vec4a_W };
+    inline constexpr Mat44 Mat44_Zero{ Vec4a_Zero, Vec4a_Zero, Vec4a_Zero, Vec4a_Zero };
+    inline constexpr Mat44 Mat44_Identity{ Vec4a_X, Vec4a_Y, Vec4a_Z, Vec4a_W };
 }

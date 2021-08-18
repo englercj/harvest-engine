24cd6e9c411fd63db61219ad2792a2e550cfead6
Chad Engler <englercj@live.com>
2021-08-18 08:52:35 -0700

    Add result formatting wrapper

---
+++ engine/core/include/he/core/result_fmt.h
@@ -0,0 +1,29 @@
+// Copyright Chad Engler
+
+#pragma once
+
+#include "he/core/allocator.h"
+#include "he/core/result.h"
+#include "he/core/string.h"
+#include "he/core/string_fmt.h"
+
+#include "fmt/format.h"
+
+namespace fmt
+{
+    template <>
+    struct formatter<he::Result>
+    {
+        constexpr auto parse(fmt::format_parse_context& ctx) -> decltype(ctx.begin())
+        {
+            return ctx.begin();
+        }
+
+        template <typename FormatContext>
+        auto format(const he::Result& result, FormatContext& ctx) -> decltype(ctx.out())
+        {
+            const String msg = result.ToString(CrtAllocator::Get());
+            return format_to(ctx.out(), "({}) {}", result.GetCode(), msg);
+        }
+    };
+}

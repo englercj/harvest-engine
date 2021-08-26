c3220f25cc34f832b0e2271bf9d8d20d99adf946
Chad Engler <englercj@live.com>
2021-08-25 19:45:56 -0700

    Add some comments for core config

--- engine/core/include/he/core/config.h
+++ engine/core/include/he/core/config.h
@@ -2,6 +2,9 @@
 
 #pragma once
 
+/// \def HE_INTERNAL_BUILD
+/// Controls if this build is considered Internal. By default this is enabled for non-Shipping
+/// configurations.
 #if !defined(HE_INTERNAL_BUILD)
     #if defined(HE_CFG_SHIPPING)
         #define HE_INTERNAL_BUILD       0
@@ -10,18 +13,14 @@
     #endif
 #endif
 
+/// \def HE_ENABLE_ASSERTIONS
+/// Controls if assertions are checked. By default these are enabled for Internal builds.
 #if !defined(HE_ENABLE_ASSERTIONS)
     #define HE_ENABLE_ASSERTIONS        HE_INTERNAL_BUILD
 #endif
 
-#if !defined(HE_ENABLE_MEMORY_TRACKING)
-    #define HE_ENABLE_MEMORY_TRACKING   HE_INTERNAL_BUILD
-#endif
-
-#if !defined(HE_ENABLE_DYNAMIC_MODULES)
-    #define HE_ENABLE_DYNAMIC_MODULES   HE_INTERNAL_BUILD
-#endif
-
+/// \def HE_ENABLE_SIMD
+/// Controls if SIMD instructions can be used. By default this is enabled.
 #if !defined(HE_ENABLE_SIMD)
     #define HE_ENABLE_SIMD              1
 #endif

daf385b654c91ec5569839ba8b7b56d45e953364
Chad Engler <englercj@live.com>
2022-06-17 18:22:35 -0700

    Mark keywords as inline

--- plugins/schema/schema_compiler/src/keywords.h
+++ plugins/schema/schema_compiler/src/keywords.h
@@ -8,42 +8,42 @@
 namespace he::schema
 {
     // Declaration and scope identifiers
-    constexpr StringView KW_Alias = "alias";
-    constexpr StringView KW_Attribute = "attribute";
-    constexpr StringView KW_Const = "const";
-    constexpr StringView KW_Enum = "enum";
-    constexpr StringView KW_Enumerator = "enumerator";
-    constexpr StringView KW_Extends = "extends";
-    constexpr StringView KW_Field = "field";
-    constexpr StringView KW_File = "file";
-    constexpr StringView KW_Group = "group";
-    constexpr StringView KW_Import = "import";
-    constexpr StringView KW_Interface = "interface";
-    constexpr StringView KW_Method = "method";
-    constexpr StringView KW_Namespace = "namespace";
-    constexpr StringView KW_Parameter = "parameter";
-    constexpr StringView KW_Stream = "stream";
-    constexpr StringView KW_Struct = "struct";
-    constexpr StringView KW_Union = "union";
+    inline constexpr StringView KW_Alias = "alias";
+    inline constexpr StringView KW_Attribute = "attribute";
+    inline constexpr StringView KW_Const = "const";
+    inline constexpr StringView KW_Enum = "enum";
+    inline constexpr StringView KW_Enumerator = "enumerator";
+    inline constexpr StringView KW_Extends = "extends";
+    inline constexpr StringView KW_Field = "field";
+    inline constexpr StringView KW_File = "file";
+    inline constexpr StringView KW_Group = "group";
+    inline constexpr StringView KW_Import = "import";
+    inline constexpr StringView KW_Interface = "interface";
+    inline constexpr StringView KW_Method = "method";
+    inline constexpr StringView KW_Namespace = "namespace";
+    inline constexpr StringView KW_Parameter = "parameter";
+    inline constexpr StringView KW_Stream = "stream";
+    inline constexpr StringView KW_Struct = "struct";
+    inline constexpr StringView KW_Union = "union";
 
     // Types
-    constexpr StringView KW_AnyPointer = "AnyPointer";
-    constexpr StringView KW_Blob = "Blob";
-    constexpr StringView KW_Bool = "bool";
-    constexpr StringView KW_Float32 = "float32";
-    constexpr StringView KW_Float64 = "float64";
-    constexpr StringView KW_Int8 = "int8";
-    constexpr StringView KW_Int16 = "int16";
-    constexpr StringView KW_Int32 = "int32";
-    constexpr StringView KW_Int64 = "int64";
-    constexpr StringView KW_Uint8 = "uint8";
-    constexpr StringView KW_Uint16 = "uint16";
-    constexpr StringView KW_Uint32 = "uint32";
-    constexpr StringView KW_Uint64 = "uint64";
-    constexpr StringView KW_String = "String";
-    constexpr StringView KW_Void = "void";
+    inline constexpr StringView KW_AnyPointer = "AnyPointer";
+    inline constexpr StringView KW_Blob = "Blob";
+    inline constexpr StringView KW_Bool = "bool";
+    inline constexpr StringView KW_Float32 = "float32";
+    inline constexpr StringView KW_Float64 = "float64";
+    inline constexpr StringView KW_Int8 = "int8";
+    inline constexpr StringView KW_Int16 = "int16";
+    inline constexpr StringView KW_Int32 = "int32";
+    inline constexpr StringView KW_Int64 = "int64";
+    inline constexpr StringView KW_Uint8 = "uint8";
+    inline constexpr StringView KW_Uint16 = "uint16";
+    inline constexpr StringView KW_Uint32 = "uint32";
+    inline constexpr StringView KW_Uint64 = "uint64";
+    inline constexpr StringView KW_String = "String";
+    inline constexpr StringView KW_Void = "void";
 
     // Values
-    constexpr StringView KW_False = "false";
-    constexpr StringView KW_True = "true";
+    inline constexpr StringView KW_False = "false";
+    inline constexpr StringView KW_True = "true";
 }

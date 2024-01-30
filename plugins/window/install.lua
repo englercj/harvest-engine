265acae408ee3cdba88565434a07e300d4f339f8
Chad Engler <englercj@live.com>
2024-01-29 16:56:05 -0800

    silence npm install for test_app

--- plugins/window/install.lua
+++ plugins/window/install.lua
@@ -4,6 +4,6 @@ return function (plugin)
     local npm_path = he.get_npmjs_path()
 
     he.cwd_push("test_app/wasm")
-    os.executef("%s install -s", npm_path)
+    os.executef("%s install --silent", npm_path)
     he.cwd_pop()
 end
